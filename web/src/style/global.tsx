edae0bbf6fe129ac4c3136d207d842fc553151d7
Igor <ig.vp@hotmail.com>
2022-02-03 16:48:43 -0300

    initial configurations done

---
+++ web/src/style/global.tsx
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from "styled-components";
+export default createGlobalStyle`*{
+    margin:0;
+    padding:0;
+    outline:0;
+    box-sizing: border-box;
+    font-family: 'Lato', sans-serif;
+    background-color: #FAFAFA;
+}`;
